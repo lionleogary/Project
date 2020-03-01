c2fbb0e250f73a872cc38c4774fe97cca42e4b81
leo <lionleogary@gms.tku.edu.tw>
2020-03-01 17:55:49 +0800

    Initial commit

---
+++ app/src/main/java/com/example/project/map.java
@@ -0,0 +1,16 @@
+package com.example.project;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class map extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.location);
+
+    }
+
+}
